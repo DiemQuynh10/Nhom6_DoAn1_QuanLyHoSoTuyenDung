9e0d6381e90bb90802dcbc90c19a02126568fe55
Dinh Thi Diem Quynh <diemquynhdinh1010@gmail.com>
2025-07-04 17:25:55 +0700

    Hoàn tất phần model, controller, migration, update database

---
+++ Nhom6_QLHoSoTuyenDung/Models/UngVien.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nhom6_QLHoSoTuyenDung.Models
+{
+    public class UngVien
+    {
+        [Key]
+        [Column("id")]
+        public string MaUngVien { get; set; }
+
+        [Required]
+        [Column("ho_ten")]
+        public string HoTen { get; set; }
+
+        [Column("ngay_sinh")]
+        [DataType(DataType.Date)]
+        public DateTime? NgaySinh { get; set; }
+
+        [Required, EmailAddress]
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Column("sdt")]
+        [Phone]
+        public string SoDienThoai { get; set; }
+
+        [Required]
+        [Column("vi_tri_ung_tuyen_id")]
+        public string ViTriUngTuyenId { get; set; }
+
+        [Column("link_cv")]
+        public string LinkCV { get; set; }
+
+        [Column("kinh_nghiem")]
+        public string KinhNghiem { get; set; }
+
+        [Column("thanh_tich")]
+        public string ThanhTich { get; set; }
+
+        [Column("mo_ta")]
+        public string MoTa { get; set; }
+
+        [Column("trang_thai")]
+        public string TrangThai { get; set; }  // VD: Moi, Da duyet, Phong van, Dat, Truot
+
+        [Column("ngay_nop")]
+        [DataType(DataType.Date)]
+        public DateTime? NgayNop { get; set; }
+
+        [Column("nguon_ung_tuyen")]
+        public string NguonUngTuyen { get; set; }
+
+        [ForeignKey("ViTriUngTuyenId")]
+        public virtual ViTriTuyenDung? ViTriUngTuyen { get; set; }
+
+        public virtual ICollection<HoSoLuuTru>? HoSoLuuTrus { get; set; }
+        public virtual ICollection<LichPhongVan>? LichPhongVans { get; set; }
+    }
+}
--- Nhom6_QLHoSoTuyenDung/Program.cs
+++ Nhom6_QLHoSoTuyenDung/Program.cs
@@ -1,3 +1,5 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 namespace Nhom6_QLHoSoTuyenDung
 {
     public class Program
@@ -5,6 +7,8 @@ namespace Nhom6_QLHoSoTuyenDung
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbContext") ?? throw new InvalidOperationException("Connection string 'AppDbContext' not found.")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
